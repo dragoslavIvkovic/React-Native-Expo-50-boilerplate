ec18f4d3295187d667609ce9808c761656b88efc
Dragoslav Ivkovic <Ivkovic.dragoslav.dragi@gmail.com>
2024-02-25 23:10:54 +0100

    task: dark theme asynch storage works

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react'
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 type ThemeContextType = {
   isDarkTheme: boolean
@@ -9,11 +10,27 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export const useThemeContext = () => useContext(ThemeContext)
 
+const THEME_STORAGE_KEY = 'userThemeIsDark'
+
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false)
 
-  const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme)
+  useEffect(() => {
+    // Učitavanje teme pri pokretanju
+    const loadTheme = async () => {
+      const storedThemePreference = await AsyncStorage.getItem(THEME_STORAGE_KEY)
+      if (storedThemePreference !== null) {
+        setIsDarkTheme(storedThemePreference === 'true')
+      }
+    }
+
+    loadTheme()
+  }, [])
+
+  const toggleTheme = async () => {
+    const newThemeValue = !isDarkTheme
+    setIsDarkTheme(newThemeValue)
+    await AsyncStorage.setItem(THEME_STORAGE_KEY, JSON.stringify(newThemeValue))
   }
 
   return (
